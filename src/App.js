9e3900b1744f99329daf99b1f9ffac2252575c6f
nduka-junior <ndukajunior4@gmail.com>
2023-03-16 19:41:34 +0100

    minor change

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ import ContextProvider from "./data/ContextProvider";
 const router = createBrowserRouter(
   [
     {
-      path: "/",
+      path: "/home",
       element: <Home />,
     },
     {
